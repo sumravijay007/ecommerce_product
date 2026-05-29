e15d0ac1a9bafe0ea9c96cbdf95939bee8985992
valiya009 <valiyachandresh33@gmail.com>
2026-05-29 09:42:29 +0530

    cros updated

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
 
+app.use(express.json());
+app.use(
+    cors({
+        origin: "https://ecommerce-product-sigma.vercel.app", // frontend URL
+        credentials: true,
+    })
+);
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log(err));
