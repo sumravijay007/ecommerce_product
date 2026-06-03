713683bf2d03958fe204ef8e627c4c074f02f362
valiya009 <valiyachandresh33@gmail.com>
2026-06-03 10:52:16 +0530

    whishlist image url chnaged

--- frontend/src/pages/Wishlist.jsx
+++ frontend/src/pages/Wishlist.jsx
@@ -58,7 +58,7 @@ function Wishlist() {
                                 <Link to={`/product/${item._id}`} className="product-link">
                                     <div className="product-image-wrapper">
                                         <img
-                                            src={`https://ecommerce-product-yww7.onrender.com/${item.images[0]}`}
+                                            src={`${item.images[0]}`}
                                             alt={item.name}
                                             className="product-image"
                                         />
